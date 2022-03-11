a1a23289bc44cacb8ed126380a3db6a963c0f8ba
Ivan Mamchits <jecter1995@gmail.com>
2022-03-11 19:38:58 +0700

    Remove magic number

--- src/test/java/ru/mamchits/informer/util/ChangesDetectorTest.java
+++ src/test/java/ru/mamchits/informer/util/ChangesDetectorTest.java
@@ -19,6 +19,8 @@ public class ChangesDetectorTest {
     private static final String NEW_URL = "new_url";
     private static final String NEW_CONTENT = "new_content";
 
+    private static final int URL_COUNT_EACH_GROUP = 1;
+
     private static final HashMap<String, String> yesterdayContent = new HashMap<>();
     private static final HashMap<String, String> todayContent = new HashMap<>();
 
@@ -26,21 +28,21 @@ public class ChangesDetectorTest {
     public void testGetNewUrls() {
         Set<String> newUrls = ChangesDetector.getNewUrls(yesterdayContent, todayContent);
         assert newUrls.contains(NEW_URL);
-        assert newUrls.size() == 1;
+        assert newUrls.size() == URL_COUNT_EACH_GROUP;
     }
 
     @Test(timeout = 100)
     public void testGetRemovedUrls() {
         Set<String> removedUrls = ChangesDetector.getRemovedUrls(yesterdayContent, todayContent);
         assert removedUrls.contains(REMOVED_URL);
-        assert removedUrls.size() == 1;
+        assert removedUrls.size() == URL_COUNT_EACH_GROUP;
     }
 
     @Test(timeout = 100)
     public void testGetModifiedUrls() {
         Set<String> modifiedUrls = ChangesDetector.getModifiedUrls(yesterdayContent, todayContent);
         assert modifiedUrls.contains(MODIFIED_URL);
-        assert modifiedUrls.size() == 1;
+        assert modifiedUrls.size() == URL_COUNT_EACH_GROUP;
     }
 
     @Before
