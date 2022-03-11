c1b1f637fb98f388a5791232534c724c4bf2b31b
Ivan Mamchits <jecter1995@gmail.com>
2022-03-11 19:51:10 +0700

    Add test for LetterMaker (testMakeWithChanges)

--- src/test/java/ru/mamchits/informer/LetterMakerTest.java
+++ src/test/java/ru/mamchits/informer/LetterMakerTest.java
@@ -14,15 +14,32 @@ public class LetterMakerTest {
     private static final String TEST_SECRETARY_PATRONYMIC = "Юрьевна";
 
     private static final String LETTER_FROM_TEMPLATE_MANUALLY_NO_CHANGES = """
-                Здравствуйте, дорогая Наталья Юрьевна!
-
-                За последние сутки во вверенных Вам сайтах изменений не произошло.
-                
-                С уважением, автоматизированная система мониторинга.""";
+                        Здравствуйте, дорогая Наталья Юрьевна!
+        
+                        За последние сутки во вверенных Вам сайтах изменений не произошло.
+                        
+                        С уважением, автоматизированная система мониторинга.""";
 
     private static final String REMOVED_URL = "removed_url";
     private static final String NEW_URL = "new_url";
     private static final String MODIFIED_URL = "modified_url";
+
+    private static final String LETTER_FROM_TEMPLATE_MANUALLY_WITH_CHANGES = """
+                        Здравствуйте, дорогая Наталья Юрьевна!
+                                            
+                        За последние сутки во вверенных Вам сайтах произошли следующие изменения:
+                        
+                        Исчезли следующие страницы:
+                        \tremoved_url
+                        
+                        Появились следующие новые страницы:
+                        \tnew_url
+                        
+                        Изменились следующие страницы:
+                        \tmodified_url
+                        
+                        С уважением, автоматизированная система мониторинга.""";
+
 
     @Test(timeout = 100)
     public void testMakeEmpty() {
@@ -48,5 +65,23 @@ public class LetterMakerTest {
                                          removedPages, newPages, modifiedPages);
 
         assert letter.equals(LETTER_FROM_TEMPLATE_MANUALLY_NO_CHANGES);
+    }
+
+    @Test(timeout = 100)
+    public void testMakeWithChanges() {
+        Set<String> removedPages = new HashSet<>();
+        removedPages.add(REMOVED_URL);
+
+        Set<String> newPages = new HashSet<>();
+        newPages.add(NEW_URL);
+
+        Set<String> modifiedPages = new HashSet<>();
+        modifiedPages.add(MODIFIED_URL);
+
+        LetterMaker letterMaker = new LetterMaker(LETTER_TEMPLATE_PROPERTIES_PATH);
+        String letter = letterMaker.make(LetterMaker.Gender.FEMALE, TEST_SECRETARY_NAME, TEST_SECRETARY_PATRONYMIC,
+                removedPages, newPages, modifiedPages);
+
+        assert letter.equals(LETTER_FROM_TEMPLATE_MANUALLY_WITH_CHANGES);
     }
 }
