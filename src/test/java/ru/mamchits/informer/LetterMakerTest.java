94bdbefc5c659f207cf6cee9e3ecf908036b47d1
Ivan Mamchits <jecter1995@gmail.com>
2022-03-11 20:18:53 +0700

    Make testMakeWithChanges2

--- src/test/java/ru/mamchits/informer/LetterMakerTest.java
+++ src/test/java/ru/mamchits/informer/LetterMakerTest.java
@@ -21,6 +21,7 @@ public class LetterMakerTest {
                         С уважением, автоматизированная система мониторинга.""";
 
     private static final String REMOVED_URL = "www.removed.com";
+    private static final String SECOND_REMOVED_URL = "www.removed2.com";
     private static final String NEW_URL = "www.new.com";
     private static final String MODIFIED_URL = "www.modified.com";
 
@@ -37,6 +38,20 @@ public class LetterMakerTest {
                         
                         Изменились следующие страницы:
                         \twww.modified.com
+                        
+                        С уважением, автоматизированная система мониторинга.""";
+
+    private static final String LETTER_FROM_TEMPLATE_MANUALLY_WITH_CHANGES_2 = """
+                        Здравствуйте, дорогая Наталья Юрьевна!
+                                            
+                        За последние сутки во вверенных Вам сайтах произошли следующие изменения:
+                        
+                        Исчезли следующие страницы:
+                        \twww.removed.com
+                        \twww.removed2.com
+                        
+                        Появились следующие новые страницы:
+                        \twww.new.com
                         
                         С уважением, автоматизированная система мониторинга.""";
 
@@ -83,5 +98,23 @@ public class LetterMakerTest {
                 removedPages, newPages, modifiedPages);
 
         assert letter.equals(LETTER_FROM_TEMPLATE_MANUALLY_WITH_CHANGES);
+    }
+
+    @Test(timeout = 100)
+    public void testMakeWithChanges2() {
+        Set<String> removedPages = new HashSet<>();
+        removedPages.add(REMOVED_URL);
+        removedPages.add(SECOND_REMOVED_URL);
+
+        Set<String> newPages = new HashSet<>();
+        newPages.add(NEW_URL);
+
+        Set<String> modifiedPages = new HashSet<>();
+
+        LetterMaker letterMaker = new LetterMaker(LETTER_TEMPLATE_PROPERTIES_PATH);
+        String letter = letterMaker.make(LetterMaker.Gender.FEMALE, TEST_SECRETARY_NAME, TEST_SECRETARY_PATRONYMIC,
+                removedPages, newPages, modifiedPages);
+
+        assert letter.equals(LETTER_FROM_TEMPLATE_MANUALLY_WITH_CHANGES_2);
     }
 }
